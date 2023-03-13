7afc81b248ef939e539047ce9dce5fe019175842
Himansha <himanshan@gmail.com>
2023-03-13 14:20:45 +0530

    first commit

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "root", "root@1234321", "bpmsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy;  SALLON Admin Panel.</p>
+    </div>
+        <!--//footer-->
